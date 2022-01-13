0f0cccc60d6ec6a195714958543f958fc0970c92
Andrzej Perczak <linux@andrzejperczak.com>
2022-01-14 00:43:29 +0100

    mm: Tune parameters for Android
    Since Infinity Kernel is released as standalone package other ROMs can
    lack mm tuning done in rootdir. For this reason move those tweaks
    directly to kernel.
    
    Note that dirty_writeback_centisecs and drity_background_ratio have to be
    restricted from writing because Android < 12 modifies these values badly
    on boot.
    
    Change-Id: I7eb7ed84c94770d454138fca7f2685346a5f663a
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -190,8 +190,7 @@ static u64 workingset_protection_prev_totalram __read_mostly = 0;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 80;
-
+int vm_swappiness = 100;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
