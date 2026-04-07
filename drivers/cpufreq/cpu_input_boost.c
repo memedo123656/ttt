374de4e256ca6065e049aabf99e0204d9dfd924b
Richard Raya <rdxzv.dev@gmail.com>
2026-04-07 19:02:02 -0300

    cpu_input_boost: Use input_boost_duration in zero-duration guard
    The early-exit check in __cpu_input_boost_kick was still referencing
    the compile-time CONFIG_INPUT_BOOST_DURATION_MS constant instead of
    the runtime input_boost_duration module param, making a userspace
    write of 0 ineffective at suppressing boosts.
    
    Change-Id: I7ec2aa7de9103669936b1fc6cd3f5dec90b17909
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -126,7 +126,7 @@ bool cpu_input_boost_within_input(unsigned long timeout_ms)
 
 static void __cpu_input_boost_kick(struct boost_drv *b)
 {
-	if (test_bit(SCREEN_OFF, &b->state) || is_battery_saver_on())
+	if (test_bit(SCREEN_OFF, &b->state) || (input_boost_duration == 0))
 		return;
 
 #ifdef CONFIG_KPROFILES
