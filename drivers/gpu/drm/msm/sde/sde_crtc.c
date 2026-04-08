59e14d3983b4f42c5626ac33602849b99aa6670b
Yahya Wessam <yahyawessam2002@gmail.com>
2026-04-08 19:32:52 +0200

    Revert "drm/msm/sde: Move frame commit boosts from drm_atomic to sde_crtc"
    This reverts commit 282564d0d12098dc83b12d8985f3993f6cf6ad43.
    
    Caused too much battery drain, gonna drop ddr bus boosting in sde_crtc aswell
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -21,7 +21,6 @@
 #include <linux/sort.h>
 #include <linux/debugfs.h>
 #include <linux/ktime.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 #include <uapi/drm/sde_drm.h>
 #include <drm/drm_mode.h>
@@ -133,14 +132,9 @@ static struct sde_crtc_custom_events custom_events[] = {
 /* default line padding ratio limitation */
 #define MAX_VPADDING_RATIO_M		63
 #define MAX_VPADDING_RATIO_N		15
-
 #define IDLE_TIMEOUT_DEFAULT		200
 
 int dim_layer_alpha;
-#ifdef CONFIG_KPROFILES
-extern int kp_active_mode(void);
-#endif
-
 static inline struct sde_kms *_sde_crtc_get_kms(struct drm_crtc *crtc)
 {
 	struct msm_drm_private *priv;
@@ -4570,29 +4564,10 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 
 	SDE_ATRACE_BEGIN("crtc_commit");
 
-#ifdef CONFIG_KPROFILES
-	switch (kp_active_mode()) {
-	case 0:
-	case 2:
-		if (cpu_input_boost_within_input(3250))
-			cpu_input_boost_kick();
-		if (df_boost_within_input(3250))
-			devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-		break;
-	case 3:
-		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-		break;
-	default:
-		break;
+	/* Boost when a new frame is ready to be committed */
+	if (df_boost_within_input(3250)) {
+                devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 	}
-#else
-	if (cpu_input_boost_within_input(3250))
-		cpu_input_boost_kick();
-
-	if (df_boost_within_input(3250))
-		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-#endif
 
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
 
